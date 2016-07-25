40f8362730f1b62576099d4c5f86fd94083d1db2
zrounds <zachary.c.rounds@gmail.com>
2016-07-25 16:10:19 -0400

    Of course I didn't change items to cards everywhere. Oops.

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,12 +4,12 @@ $(document).ready(function(){
 	$.getJSON( "https://api.hearthstonejson.com/v1/latest/enUS/cards.collectible.json", function( data ) {
 		
 		$.each( data, function( i ) {
-			items.push( "<li id='" + data[i]["id"] + "'>" + data[i]["name"] + "</li>" );
+			cards.push( "<li id='" + data[i]["id"] + "'>" + data[i]["name"] + "</li>" );
 		});
 		
 		$( "<ul/>", {
 			"class": "my-new-list",
-		html: items.join( "" )
+		html: cards.join( "" )
 		}).appendTo("#collection");
 	});
 });
