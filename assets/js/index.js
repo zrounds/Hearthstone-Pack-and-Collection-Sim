7ab984d5f744cf2681bc745d4b378963a8d7453e
zrounds <zachary.c.rounds@gmail.com>
2016-07-26 14:45:22 -0400

    Working...

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,16 @@
 ﻿var cards = [];
-var sets = [];
+var collection = {};
+var sets = ["EXPERT1", "GVG", "TGT", "OG"];
+var setsToSkip = ["CORE","HERO_SKINS", "PROMO", "REWARD"]; 
+var adventureSets = ["LOE", "BRM", "NAXX"];
+var setNames = {"EXPERT1":"Classic", 
+				"CORE":"Basic", 
+				"GVG": "Goblins Versus Gnomes", 
+				"TGT":"The Grand Tournament",
+				"OG":"Whispers of the Old Gods",
+				"LOE":"League of Explorers",
+				"BRM":"Black Rock Mountain",
+				"NAXX":"Curse of Naxxramus"}
 
 $(document).ready(function(){
 	
@@ -11,17 +22,39 @@ $(document).ready(function(){
 			cards = data.slice();
         }
     });
-	$.each(cards, function(i) {
-		if ($.inArray(cards[i]["set"], sets) < 0){
+	//Defunct - I decided to hard-code the sets because the API does not put a distinction b/w adventures and expansions and I need the distinction 
+	/*$.each(cards, function(i) {
+		if ($.inArray(cards[i]["set"], sets) < 0 && $.inArray(cards[i]["set"], setsToSkip) < 0){
 			sets.push(cards[i]["set"]);
 		}
-	});
+	});*/
 	$.each(sets, function(i){
-		$("#selectSet").append("<input type = 'radio' class = 'sets' name = 'sets' id = '" + sets[i] + "' value = '" + sets[i] + "'> " + sets[i] +  ((i < sets.length - 1) ? " | " : ""));
+		$("#selectSet").append("<input type = 'radio' class = 'sets' name = 'sets' id = '" + sets[i] + "' value = '" + sets[i] + "'> " + setNames[sets[i]] +  ((i < sets.length - 1) ? " | " : ""));
 	});
 	$("#"+sets[0]).prop("checked", true);
+	$.each(cards, function(i){
+		collection[cards[i]["id"]] = 0; 
+	});
 });
 
 function buyPacks(){
-	$("#collection").append("You bought " + $(".numPacks:checked").val() + " packs from the " + $(".sets:checked").val() + " set.<br>");
+	
+	//Subset of only the chosen set's cards
+	var selectedSet = cards.filter(isCurrentlySelectedSet);
+	console.log(getRandomArbitrary(0,100));
+	
+	
+	$("#collection").append("You bought " + $(".numPacks:checked").val() + " packs from the " + setNames[$(".sets:checked").val()] + " set.<br>");
+}
+
+function isCurrentlySelectedSet(value){
+	return value["set"] == $(".sets:checked").val();
+}
+
+function isGreaterThanZero(value){
+	return value > 0; 
+}
+
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
 }
