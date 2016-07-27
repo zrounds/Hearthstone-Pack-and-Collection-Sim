beadbdfc750f3ef58ce790a3b44d8a38c5da4cd7
zrounds <zachary.c.rounds@gmail.com>
2016-07-27 14:31:16 -0400

    rarity sets

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,4 @@
-﻿var cards = [];
+﻿var cards = {};
 var collection = {};
 var sets = ["EXPERT1", "GVG", "TGT", "OG"];
 var setsToSkip = ["CORE","HERO_SKINS", "PROMO", "REWARD"]; 
@@ -42,10 +42,46 @@ $(document).ready(function(){
 });
 
 function buyPacks(){
-	
-	//Subset of only the chosen set's cards
-	var selectedSet = cards.filter(isCurrentlySelectedSet);
+	//Create subsets of only the chosen set's cards, broken into rarities for convenience 
+	var commonCards = [], rareCards = [], epicCards = [], legendaryCards = [];
+	for (var key in cards){
+		if (isCurrentlySelectedSet(cards[key])) {
+			switch(cards[key]["rarity"]){
+				case "COMMON":
+					commonCards.push(cards[key]);
+					break;
+				case "RARE":
+					rareCards.push(cards[key]);
+					break;
+				case "EPIC":
+					epicCards.push(cards[key]);
+					break;
+				case "LEGENDARY":
+					legendaryCards.push(cards[key]);
+					break;
+			}
+		}
+	}
+	console.clear();
+	console.log(commonCards);
+	console.log(rareCards);
+	console.log(epicCards);
+	console.log(legendaryCards);
 	console.log(getRandomArbitrary(0,100));
+	
+	//Determine rarity of 1-4
+	//If none are better than common, restrict last card to rare or better 
+	//Determine final rarity 
+	for (var i = 0; i < $(".numPacks:checked").val(); i++){
+		var rarities = [];
+		for (var j=0; j<4; j++) rarities.push(getRandomArbitrary(0,100));
+		if(rarities[0] < 5 && rarities[1] < 5 && rarities[2] < 5 && rarities[3] < 5){
+			rarities.push();
+		} else {
+			rarities.push(getRandomArbitrary(0,100));
+		}
+		//Distribute random cards based on rarity
+	}
 	
 	//Dump collection
 	$("#collection").html("");
@@ -53,13 +89,29 @@ function buyPacks(){
 		if (collection[key] > 0) $("#collection").append(cards[key]["name"] + ": " + collection[key] + "<br>");
 	};
 	
+	$("#history").prepend($("#message").html());
 	$("#message").html("You bought " + $(".numPacks:checked").val() + " packs from the " + setNames[$(".sets:checked").val()] + " set.<br>");
-	$("#history").append("You bought " + $(".numPacks:checked").val() + " packs from the " + setNames[$(".sets:checked").val()] + " set.<br>");
 }
 
-function isCurrentlySelectedSet(value){
+function isCurrentlySelectedSet(value,i){
 	return value["set"] == $(".sets:checked").val();
 }
+/*
+function isCommon(value){
+	return value["rarity"] == "COMMON";
+}
+
+function isRare(value){
+	return value["rarity"] == "RARE";
+}
+
+function isEpic(value){
+	return value["rarity"] == "EPIC";
+}
+
+function isLegendary(value){
+	return value["rarity"] == "LEGENDARY";
+}*/
 
 function isGreaterThanZero(value){
 	return collection[value] > 0; 
